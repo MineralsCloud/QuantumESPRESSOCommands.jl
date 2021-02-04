03513edb7544e8553654e404a9d35ed9aae0622c
singularitti <singularitti@outlook.com>
2021-02-04 01:27:33 -0500

    Add type `PwConfig` & `PwXConfig`

--- src/QuantumESPRESSOCli.jl
+++ src/QuantumESPRESSOCli.jl
@@ -1,5 +1,23 @@
 module QuantumESPRESSOCli
 
-# Write your package code here.
+using AbInitioSoftwareBase
+using Comonicon: @cast, @main
+using Configurations: from_kwargs, from_dict, @option
+
+@option struct PwConfig
+    exe::String = "pw.x"
+    script_dest::String = ""
+    chdir::Bool = true
+    options::PwXConfig = PwXConfig()
+end
+
+@option struct PwXConfig
+    nimage::UInt = 0
+    npool::UInt = 0
+    ntg::UInt = 0
+    nyfft::UInt = 0
+    nband::UInt = 0
+    ndiag::UInt = 0
+end
 
 end
