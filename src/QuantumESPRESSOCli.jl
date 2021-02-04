9696517b645b261acda3d945d047baf691154f09
singularitti <singularitti@outlook.com>
2021-02-04 01:28:05 -0500

    Move `scriptify` from QuantumESPRESSOBase.CLI to here

--- src/QuantumESPRESSOCli.jl
+++ src/QuantumESPRESSOCli.jl
@@ -20,4 +20,36 @@ end
     ndiag::UInt = 0
 end
 
+function scriptify(input, output, error, options::PwConfig)
+    args = [options.exe]
+    for f in fieldnames(PwXConfig)
+        v = getfield(options, f)
+        if !iszero(v)
+            push!(args, "-$f", string(v))
+        end
+    end
+    dir = expanduser(dirname(input))
+    if !isempty(options.script_dest)
+        for (k, v) in zip(("-inp", "1>", "2>"), (input, output, error))
+            if v !== nothing
+                push!(args, k, "'$v'")
+            end
+        end
+        if !isdir(dir)
+            mkpath(dir)
+        end
+        str = join(args, " ")
+        write(options.script_dest, str)
+        chmod(options.script_dest, 0o755)
+        return setenv(Cmd([abspath(options.script_dest)]); dir = dir)
+    else
+        push!(args, "-inp", "$input")
+        return pipeline(setenv(Cmd(args); dir = dir), stdout = output, stderr = error)
+    end
+    return args
+end
+scriptify(input, output, error, options::AbstractDict) =
+    scriptify(input, output, error, from_dict(PwConfig, options))
+scriptify(input, output, error; kwargs...) =
+    scriptify(input, output, error, from_kwargs(PwConfig; kwargs...))
 end
