46f627dc89b631d60038aaef3ba12ecbb29222c9
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-29 04:52:28 -0400

    Rename in docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,16 @@
-using QuantumESPRESSOCli
+using QuantumESPRESSOCommands
 using Documenter
 
+DocMeta.setdocmeta!(QuantumESPRESSOCommands, :DocTestSetup, :(using QuantumESPRESSOCommands); recursive=true)
+
 makedocs(;
-    modules=[QuantumESPRESSOCli],
+    modules=[QuantumESPRESSOCommands],
     authors="Qi Zhang <singularitti@outlook.com>",
-    repo="https://github.com/MineralsCloud/QuantumESPRESSOCli.jl/blob/{commit}{path}#L{line}",
-    sitename="QuantumESPRESSOCli.jl",
+    repo="https://github.com/MineralsCloud/QuantumESPRESSOCommands.jl/blob/{commit}{path}#{line}",
+    sitename="QuantumESPRESSOCommands.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://MineralsCloud.github.io/QuantumESPRESSOCli.jl",
+        canonical="https://MineralsCloud.github.io/QuantumESPRESSOCommands.jl",
         assets=String[],
     ),
     pages=[
@@ -17,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/MineralsCloud/QuantumESPRESSOCli.jl",
+    repo="github.com/MineralsCloud/QuantumESPRESSOCommands.jl",
 )
