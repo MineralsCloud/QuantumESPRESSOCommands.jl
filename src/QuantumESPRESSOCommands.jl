c564bfa1c125017c492a4846b0b8adeaed63228c
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-30 00:00:49 -0400

    Rename variable `options` to `main` in `makecmd`

--- src/QuantumESPRESSOCommands.jl
+++ src/QuantumESPRESSOCommands.jl
@@ -54,25 +54,13 @@ end
 
 @cast function pw(input, output = tempname(; cleanup = false), error = output; cfgfile = "")
     config = materialize(cfgfile)
-    cmd = makecmd(
-        input;
-        output = output,
-        error = error,
-        mpi = config.mpi,
-        options = config.pw,
-    )
+    cmd = makecmd(input; output = output, error = error, mpi = config.mpi, main = config.pw)
     return run(cmd)
 end
 
 @cast function ph(input, output = tempname(; cleanup = false), error = output; cfgfile = "")
     config = materialize(cfgfile)
-    cmd = makecmd(
-        input;
-        output = output,
-        error = error,
-        mpi = config.mpi,
-        options = config.ph,
-    )
+    cmd = makecmd(input; output = output, error = error, mpi = config.mpi, main = config.ph)
     return run(cmd)
 end
 
@@ -83,13 +71,8 @@ end
     cfgfile = "",
 )
     config = materialize(cfgfile)
-    cmd = makecmd(
-        input;
-        output = output,
-        error = error,
-        mpi = config.mpi,
-        options = config.q2r,
-    )
+    cmd =
+        makecmd(input; output = output, error = error, mpi = config.mpi, main = config.q2r)
     return run(cmd)
 end
 
@@ -105,7 +88,7 @@ end
         output = output,
         error = error,
         mpi = config.mpi,
-        options = config.matdyn,
+        main = config.matdyn,
     )
     return run(cmd)
 end
@@ -124,25 +107,25 @@ function makecmd(
     output = tempname(; cleanup = false),
     error = "",
     mpi = MpiexecConfig(),
-    options = PwxConfig(),
+    main,
 )
     if mpi.np == 0
-        args = [options.exe]
+        args = [main.exe]
     else
         args = [mpi.exe, "-n", string(mpi.np)]
         for (k, v) in mpi.options
             push!(args, k, string(v))
         end
-        push!(args, options.exe)
+        push!(args, main.exe)
     end
-        v = getfield(options.options, f)
     for f in fieldnames(ParallelizationFlags)
+        v = getfield(main.options, f)
         if !iszero(v)
             push!(args, "-$f", string(v))
         end
     end
     dir = expanduser(dirname(input))
-    if !isempty(options.script_dest)
+    if !isempty(main.script_dest)
         for (k, v) in zip(("-inp", "1>", "2>"), (input, output, error))
             if v !== nothing
                 push!(args, k, "'$v'")
@@ -152,9 +135,9 @@ function makecmd(
             mkpath(dir)
         end
         str = join(args, " ")
-        write(options.script_dest, str)
-        chmod(options.script_dest, 0o755)
-        return setenv(Cmd([abspath(options.script_dest)]); dir = dir)
+        write(main.script_dest, str)
+        chmod(main.script_dest, 0o755)
+        return setenv(Cmd([abspath(main.script_dest)]); dir = dir)
     else
         push!(args, "-inp", "$input")
         return pipeline(setenv(Cmd(args); dir = dir), stdout = output, stderr = error)
