8585d0586c3c66821da329858f9b8252934d3ef2
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-11 11:26:12 -0400

    Add some tests to `makecmd`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,43 @@
+using AbInitioSoftwareBase.Commands: MpiexecConfig
 using QuantumESPRESSOCommands
 using Test
 
 @testset "QuantumESPRESSOCommands.jl" begin
-    # Write your tests here.
+    @test makecmd(
+        "scf.in";
+        output = "scf.out",
+        mpi = MpiexecConfig(; np = 8),
+        main = PwxConfig(),
+        use_script = false,
+    ).cmd.cmd.exec == ["mpiexec", "-n", "8", "pw.x", "-inp", "scf.in"]
+    file = first(
+        makecmd(
+            "scf.in";
+            output = "scf.out",
+            mpi = MpiexecConfig(; np = 8),
+            main = PwxConfig(),
+            use_script = true,
+        ).exec,
+    )
+    @test isfile(file)
+    @test read(file, String) == "mpiexec -n 8 pw.x -inp 'scf.in' 1> 'scf.out' 2> 'scf.out'"
+    @test makecmd(
+        "scf.in";
+        output = "scf.out",
+        mpi = MpiexecConfig(; np = 8),
+        main = PwxConfig(; options = ParallelizationFlags(; nimage = 8)),
+        use_script = false,
+    ).cmd.cmd.exec == ["mpiexec", "-n", "8", "pw.x", "-nimage", "8", "-inp", "scf.in"]
+    file = first(
+        makecmd(
+            "scf.in";
+            output = "scf.out",
+            error = "scf.err",
+            mpi = MpiexecConfig(),
+            main = PwxConfig(; options = ParallelizationFlags(; nimage = 8)),
+            use_script = true,
+        ).exec,
+    )
+    @test isfile(file)
+    @test read(file, String) == "pw.x -nimage 8 -inp 'scf.in' 1> 'scf.out' 2> 'scf.err'"
 end
