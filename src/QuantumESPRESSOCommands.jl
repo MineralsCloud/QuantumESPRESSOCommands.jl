800691d33a5a733c87ae9e4d198e23f3115156f2
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-19 05:04:27 -0400

    Fix `ph`

--- src/QuantumESPRESSOCommands.jl
+++ src/QuantumESPRESSOCommands.jl
@@ -189,21 +189,27 @@ Run command `ph.x`.
     input,
     output = mktemp(parentdir(input))[1],
     error = output;
+    np = 0,
+    exe = "ph.x",
+    chdir = true,
     use_script = false,
-    cfgfile = "",
 )
-    mpi, main = if isempty(cfgfile)
-        MpiexecConfig(), PhxConfig()
-    else
-        config = readconfig(cfgfile)
-        config.mpi, config.ph
-    end
+    mpi = MpiexecConfig(; np = np)
+    main = PhxConfig(; exe = exe, chdir = chdir, use_script = use_script)
+    return pw(input, output, error, mpi, main)
+end
+function ph(
+    input,
+    output = mktemp(parentdir(input))[1],
+    error = output,
+    mpi = MpiexecConfig(),
+    main = PhxConfig(),
+)
     cmd = makecmd(
         input;
         output = output,
         error = error,
         dir = main.chdir ? parentdir(input) : pwd(),  # See https://github.com/MineralsCloud/QuantumESPRESSOCommands.jl/pull/10
-        use_script = use_script,
         mpi = mpi,
         main = main,
     )
