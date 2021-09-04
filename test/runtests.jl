6e83f58d65c8ddce28f28873513e1ad7c1b2713c
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-04 08:05:46 -0400

    Fix test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,39 +7,12 @@ using Test
         "scf.in";
         output = "scf.out",
         mpi = MpiexecConfig(; np = 8),
-        main = PwxConfig(; use_script = false),
-    ).cmd.cmd.exec == ["mpiexec", "-n", "8", "pw.x", "-inp", "scf.in"]
-    file = first(
-        makecmd(
-            "scf.in";
-            output = "scf.out",
-            mpi = MpiexecConfig(; np = 8),
-            main = PwxConfig(; use_script = true),
-        ).exec,
-    )
-    @test isfile(file)
-    @test read(file, String) == "mpiexec -n 8 pw.x -inp 'scf.in' 1> 'scf.out' 2> 'scf.out'"
+        main = PwxConfig(),
+    ).cmd.cmd.cmd.exec[2:end] == ["-np", "8", "pw.x"]
     @test makecmd(
         "scf.in";
         output = "scf.out",
         mpi = MpiexecConfig(; np = 8),
-        main = PwxConfig(;
-            options = ParallelizationFlags(; nimage = 8),
-            use_script = false,
-        ),
-    ).cmd.cmd.exec == ["mpiexec", "-n", "8", "pw.x", "-nimage", "8", "-inp", "scf.in"]
-    file = first(
-        makecmd(
-            "scf.in";
-            output = "scf.out",
-            error = "scf.err",
-            mpi = MpiexecConfig(),
-            main = PwxConfig(;
-                options = ParallelizationFlags(; nimage = 8),
-                use_script = true,
-            ),
-        ).exec,
-    )
-    @test isfile(file)
-    @test read(file, String) == "pw.x -nimage 8 -inp 'scf.in' 1> 'scf.out' 2> 'scf.err'"
+        main = PwxConfig(; options = ParallelizationFlags(; nimage = 4)),
+    ).cmd.cmd.cmd.exec[2:end] == ["-np", "8", "pw.x", "-nimage", "4"]
 end
