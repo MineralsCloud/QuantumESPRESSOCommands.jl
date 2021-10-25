6cfde45fb796587b1064c1499fa78aadbb17a430
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-25 05:36:04 -0400

    Fix test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,15 @@
-using AbInitioSoftwareBase.Commands: MpiexecConfig
-using QuantumESPRESSOCommands: ParallelizationFlags, PwxConfig, PhxConfig, makecmd
+using QuantumESPRESSOCommands: pw, set_path
 using Test
 
-@testset "QuantumESPRESSOCommands.jl" begin
-    @test makecmd(
-        "scf.in",
-        "scf.out";
-        mpi = MpiexecConfig(; np = 8),
-        main = PwxConfig(),
-    ).cmd.cmd.exec[2:end] == ["-np", "8", "pw.x"]
-    @test makecmd(
-        "scf.in",
-        "scf.out";
-        mpi = MpiexecConfig(; np = 8),
-        main = PwxConfig(; options = ParallelizationFlags(; nimage = 4)),
-    ).cmd.cmd.exec[2:end] == ["-np", "8", "pw.x", "-nimage", "4"]
-    @test makecmd(
-        "/home/test/ph.in",
-        "/home/test/ph.out";
-        main = PhxConfig(),
-    ).cmd.cmd.dir == "/home/test"
+@testset "Test `pw`" begin
+    @test pw("scf.in", "scf.out"; np = 8, nimage = 4, npool = 2).cmd.cmd.exec ==
+          ["mpiexec", "-np", "8", "pw.x", "-nimage", "4", "-npool", "2"]
+    @test pw("/home/test/ph.in", "/home/test/ph.out").cmd.cmd.dir == "/home/test"
+end
+
+@testset "Test `set_path`" begin
+    get_path("pw") == "pw.x"
+    touch("pw2.x")
+    set_path("pw", "pw2.x")
+    @test pw("scf.in", "scf.out"; np = 8).cmd.cmd.exec == ["mpiexec", "-np", "8", "pw2.x"]
 end
