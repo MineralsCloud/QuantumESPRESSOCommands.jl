61ba0fb929cb09e444e13ff92a09e2c11437a6a3
singularitti <singularitti@outlook.com>
2023-06-25 02:58:44 -0400

    * feat(QuantumESPRESSOCommands.jl): add support for bands.x and dos.x executables in getpath, setpath, and unsetpath functions

--- src/QuantumESPRESSOCommands.jl
+++ src/QuantumESPRESSOCommands.jl
@@ -5,11 +5,15 @@ using Preferences: @load_preference, @set_preferences!, @delete_preferences!
 
 export pwx, phx, q2rx, matdynx, dynmatx, getpath, setpath, unsetpath
 
-@enum Executable pwx phx q2rx matdynx dynmatx
+@enum Executable pwx bandsx dosx phx q2rx matdynx dynmatx
 
 function getpath(exe::Executable)
     if exe == pwx
         return @load_preference("pw.x path", "pw.x")
+    elseif exe == bandsx
+        return @load_preference("bands.x path", "bands.x")
+    elseif exe == dosx
+        return @load_preference("dos.x path", "dos.x")
     elseif exe == phx
         return @load_preference("ph.x path", "ph.x")
     elseif exe == q2rx
@@ -27,6 +31,10 @@ function setpath(exe::Executable, path::String)
     @assert ispath(path)
     if exe == pwx
         @set_preferences!("pw.x path" => path)
+    elseif exe == bandsx
+        @set_preferences!("bands.x path" => path)
+    elseif exe == dosx
+        @set_preferences!("dos.x path" => path)
     elseif exe == phx
         @set_preferences!("ph.x path" => path)
     elseif exe == q2rx
@@ -42,15 +50,19 @@ end
 
 function unsetpath(exe::Executable)
     if exe == pwx
-        return @delete_preferences!("pw.x path")
+        @delete_preferences!("pw.x path")
+    elseif exe == bandsx
+        @delete_preferences!("bands.x path")
+    elseif exe == dosx
+        @delete_preferences!("dos.x path")
     elseif exe == phx
-        return @delete_preferences!("ph.x path")
+        @delete_preferences!("ph.x path")
     elseif exe == q2rx
-        return @delete_preferences!("q2r.x path")
+        @delete_preferences!("q2r.x path")
     elseif exe == matdynx
-        return @delete_preferences!("matdyn.x path")
+        @delete_preferences!("matdyn.x path")
     elseif exe == dynmatx
-        return @delete_preferences!("dynmat.x path")
+        @delete_preferences!("dynmat.x path")
     else
         error("this should not happen!")
     end
