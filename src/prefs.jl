421ffe2bb8b0e47a74810c3bd2a74d9832883f83
singularitti <singularitti@outlook.com>
2023-06-25 03:20:07 -0400

    Rename all `exe` to `exec` in src/prefs.jl

--- src/prefs.jl
+++ src/prefs.jl
@@ -4,61 +4,61 @@ export pwx, phx, q2rx, matdynx, dynmatx, getpath, setpath, unsetpath
 
 @enum Executable pwx bandsx dosx phx q2rx matdynx dynmatx
 
-function getpath(exe::Executable)
-    if exe == pwx
+function getpath(exec::Executable)
+    if exec == pwx
         return @load_preference("pw.x path", "pw.x")
-    elseif exe == bandsx
+    elseif exec == bandsx
         return @load_preference("bands.x path", "bands.x")
-    elseif exe == dosx
+    elseif exec == dosx
         return @load_preference("dos.x path", "dos.x")
-    elseif exe == phx
+    elseif exec == phx
         return @load_preference("ph.x path", "ph.x")
-    elseif exe == q2rx
+    elseif exec == q2rx
         return @load_preference("q2r.x path", "q2r.x")
-    elseif exe == matdynx
+    elseif exec == matdynx
         return @load_preference("matdyn.x path", "matdyn.x")
-    elseif exe == dynmatx
+    elseif exec == dynmatx
         return @load_preference("dynmat.x path", "dynmat.x")
     else
         error("this should not happen!")
     end
 end
 
-function setpath(exe::Executable, path::String)
+function setpath(exec::Executable, path::String)
     @assert ispath(path)
-    if exe == pwx
+    if exec == pwx
         @set_preferences!("pw.x path" => path)
-    elseif exe == bandsx
+    elseif exec == bandsx
         @set_preferences!("bands.x path" => path)
-    elseif exe == dosx
+    elseif exec == dosx
         @set_preferences!("dos.x path" => path)
-    elseif exe == phx
+    elseif exec == phx
         @set_preferences!("ph.x path" => path)
-    elseif exe == q2rx
+    elseif exec == q2rx
         @set_preferences!("q2r.x path" => path)
-    elseif exe == matdynx
+    elseif exec == matdynx
         @set_preferences!("matdyn.x path" => path)
-    elseif exe == dynmatx
+    elseif exec == dynmatx
         @set_preferences!("dynmat.x path" => path)
     else
         error("this should not happen!")
     end
 end
 
-function unsetpath(exe::Executable)
-    if exe == pwx
+function unsetpath(exec::Executable)
+    if exec == pwx
         @delete_preferences!("pw.x path")
-    elseif exe == bandsx
+    elseif exec == bandsx
         @delete_preferences!("bands.x path")
-    elseif exe == dosx
+    elseif exec == dosx
         @delete_preferences!("dos.x path")
-    elseif exe == phx
+    elseif exec == phx
         @delete_preferences!("ph.x path")
-    elseif exe == q2rx
+    elseif exec == q2rx
         @delete_preferences!("q2r.x path")
-    elseif exe == matdynx
+    elseif exec == matdynx
         @delete_preferences!("matdyn.x path")
-    elseif exe == dynmatx
+    elseif exec == dynmatx
         @delete_preferences!("dynmat.x path")
     else
         error("this should not happen!")
